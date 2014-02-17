661be84650f6530444f9e96528821e9d88ff07f8
enthalpy-yan <enthalpy.yan@gmail.com>
2014-02-17 18:26:19 -0500

    Update basic_lbp.py

--- basic_lbp.py
+++ basic_lbp.py
@@ -1,50 +1,61 @@
 import numpy as np
 import cv2
 
-
+#
+# Compare the center pixel to its 8 neighbors
+#
 def thresholded(center, pixels):
-    out = []
+    ret = []
     for a in pixels:
         if a >= center:
-            out.append(1)
+            ret.append(1)
         else:
-            out.append(0)
-    return out
+            ret.append(0)
+    return ret
 
-def get_pixel_else_0(l, idx, idy, default=0):
+#
+# Given the center position to find the position of its neighbors.
+#
+def getNeighborhood(img, idx, idy, default=0):
     try:
-        return l[idx,idy]
+        return img[idx,idy]
     except IndexError:
         return default
 
-def basic_lbp(img, transformed_img):
+#
+# Compute LBP feature vector
+#
+def basic_lbp(img):
+    height, width = img.shape
+    ret = np.empty([height, width], np.uint8)
     for x in range(0, len(img)):
         for y in range(0, len(img[0])):
             center        = img[x,y]
-            top_left      = get_pixel_else_0(img, x-1, y-1)
-            top_up        = get_pixel_else_0(img, x, y-1)
-            top_right     = get_pixel_else_0(img, x+1, y-1)
-            right         = get_pixel_else_0(img, x+1, y )
-            left          = get_pixel_else_0(img, x-1, y )
-            bottom_left   = get_pixel_else_0(img, x-1, y+1)
-            bottom_right  = get_pixel_else_0(img, x+1, y+1)
-            bottom_down   = get_pixel_else_0(img, x,   y+1 )
+            top_left      = getNeighborhood(img, x-1, y-1)
+            top_middle    = getNeighborhood(img, x, y-1)
+            top_right     = getNeighborhood(img, x+1, y-1)
+            right         = getNeighborhood(img, x+1, y )
+            left          = getNeighborhood(img, x-1, y )
+            bottom_left   = getNeighborhood(img, x-1, y+1)
+            bottom_right  = getNeighborhood(img, x+1, y+1)
+            bottom_middle = getNeighborhood(img, x,   y+1)
 
-            values = thresholded(center, [top_left, top_up, top_right, right, bottom_right,
-                                          bottom_down, bottom_left, left])
+            values = thresholded(center, [top_left, top_middle, top_right, right, bottom_right,
+                                          bottom_middle, bottom_left, left])
 
             weights = [1, 2, 4, 8, 16, 32, 64, 128]
             res = 0
             for a in range(0, len(values)):
                 res += weights[a] * values[a]
+            ret.itemset((x,y), res)
+    return ret
 
-            transformed_img.itemset((x,y), res)
-
+#
+# Test case
+#
 img = cv2.imread('lena.jpg', 0)
-transformed_img = cv2.imread('lena.jpg', 0)
-basic_lbp(img, transformed_img)
-print img
-print transformed_img
+transformed_img = basic_lbp(img)
+
 cv2.imshow('image', img)
 cv2.imshow('thresholded image', transformed_img)
 
