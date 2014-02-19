042ebd2260962cda8d88fef91deb3c09323b0e3d
enthalpy-yan <enthalpy-yan@users.noreply.github.com>
2014-02-18 23:05:53 -0500

    Update DistanceMeasures.py

--- DistanceMeasures.py
+++ DistanceMeasures.py
@@ -1,13 +1,25 @@
 import numpy as np
 
+#
+# L1 Distance
+#
 def L1Dist(i1, i2):
     return np.sum(np.abs(img1 - img2))
 
+#
+# L2 Distance
+#
 def L2Dist(i1, i2):
     return np.sqrt(np.sum((i1 - i2) ** 2))
 
+#
+# Chi-Square Distance
+#
 def ChiSquareDist(i1, i2):
     return np.sum(2 * ((i1 - i2) ** 2) / (i1 + i2))
 
+#
+# KL Distance
+#
 def KLDist(i1, i2):
     return np.sum(np.where(i1 != 0, i1 * np.log(i1 / i2), 0))
