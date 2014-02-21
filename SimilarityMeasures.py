7f568d539c93f05634ba2fb96079956e72729928
minhui <guminhui@hotmail.com>
2014-02-21 14:27:47 -0500

    add histogram intersection 	modified:   SimilarityMeasures.py

--- SimilarityMeasures.py
+++ SimilarityMeasures.py
@@ -5,3 +5,8 @@ def CosineSimilarity(i1, i2):
     denoma = (i1 * i1).sum()
     denomb = (i2 * i2).sum()
     return 1 - numerator / np.sqrt(denoma*denomb)
+
+def hist_intersection(A,B):	
+	min_matrix = np.where(A>=B,B,0)+np.where(A<B,A,0)
+	the_min = min_matrix / float(min(np.sum(A.ravel()),np.sum(B.ravel())))
+	return  sum(the_min.ravel())
