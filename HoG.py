0eeb2cac659b5da81eabb6f9de95ca9eb5d61cd8
minhui <guminhui@hotmail.com>
2014-02-21 10:46:16 -0500

    modified:   HoG.py

--- HoG.py
+++ HoG.py
@@ -21,22 +21,20 @@ def HoG(img, cell_per_blk=(3,3), pix_per_cell=(8, 8), orientation = 3):
 	magnitude = sqrt(gradient_x ** 2 + gradient_y ** 2)
 	ori = arctan2(gradient_y, (gradient_x + 1e-15)) * (180 / pi) + 90
 	
+	# Orientation Binning
 	img_h,img_w = img.shape
 	cx, cy = pix_per_cell
 	bx, by = cell_per_blk
 	ncell_x = int(np.floor(img_w//cx))
 	ncell_y = int(np.floor(img_h//cy))
-
 	ori_histogram = np.zeros((ncell_y,ncell_x,orientation))
-
-	# Orientation Binning
 	for i in range(0, orientation):
 		temp1 = np.where(ori < 180 / orientation * (i + 1), ori, 0)
 		temp1 = np.where(ori >= 180 / orientation * i,temp1, 0)
 		temp2 = np.where(temp1>0, magnitude, 0)
 		ori_histogram[:,:,i] = uniform_filter(temp2, size=(cy,cx))[cy/2::cy, cx/2::cx]
 
-    # normalization
+	# normalization
 	n_blocksx = (ncell_x - bx) + 1
 	n_blocksy = (ncell_y - by) + 1
 	normalised_blocks = np.zeros((n_blocksy, n_blocksx, by, bx, orientation))
