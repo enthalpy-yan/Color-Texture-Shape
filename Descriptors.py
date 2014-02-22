b681429da6dcf1b2552d1a0e1c84711fea28d315
enthalpy-yan <enthalpy-yan@users.noreply.github.com>
2014-02-22 15:23:25 -0500

    Update Descriptors.py

--- Descriptors.py
+++ Descriptors.py
@@ -186,7 +186,7 @@ def lbp(img):
     return ret
 
 
-def HoG(img, cell_per_blk=(3,3), pix_per_cell=(8, 8), orientation = 3):
+def HoG(img, cell_per_blk=(3, 3), pix_per_cell=(8, 8), orientation=3):
     """
     The function for computing Histogram of oriented gradients. Takes 
     the following as keyword arguments:
@@ -209,25 +209,25 @@ def HoG(img, cell_per_blk=(3,3), pix_per_cell=(8, 8), orientation = 3):
         return -1
 
     # gradient computation
-    gradient_x =np.zeros(img.shape)
-    gradient_y =np.zeros(img.shape)
-    gradient_x[:, :-1] = np.diff(img, n=1, axis=1)
-    gradient_y[:-1, :] = np.diff(img, n=1, axis=0)
+    gradient_x = np.zeros(img.shape)
+    gradient_y = np.zeros(img.shape)
+    gradient_x[:, :-1] = np.diff(img, n = 1, axis = 1)
+    gradient_y[:-1, :] = np.diff(img, n = 1, axis = 0)
     magnitude = sqrt(gradient_x ** 2 + gradient_y ** 2)
     ori = arctan2(gradient_y, (gradient_x + 1e-15)) * (180 / pi) + 90
     
     # Orientation Binning
-    img_h,img_w = img.shape
+    img_h, img_w = img.shape
     cx, cy = pix_per_cell
     bx, by = cell_per_blk
-    ncell_x = int(np.floor(img_w//cx))
-    ncell_y = int(np.floor(img_h//cy))
-    ori_histogram = np.zeros((ncell_y,ncell_x,orientation))
+    ncell_x = int(np.floor(img_w // cx))
+    ncell_y = int(np.floor(img_h // cy))
+    ori_histogram = np.zeros((ncell_y, ncell_x, orientation))
     for i in range(0, orientation):
         temp1 = np.where(ori < 180 / orientation * (i + 1), ori, 0)
-        temp1 = np.where(ori >= 180 / orientation * i,temp1, 0)
+        temp1 = np.where(ori >= 180 / orientation * i, temp1, 0)
         temp2 = np.where(temp1>0, magnitude, 0)
-        ori_histogram[:,:,i] = uniform_filter(temp2, size=(cy,cx))[cy/2::cy, cx/2::cx]
+        ori_histogram[:,:,i] = uniform_filter(temp2, size = (cy, cx))[cy / 2 :: cy, cx / 2 :: cx]
 
     # normalization
     n_blocksx = (ncell_x - bx) + 1
@@ -235,7 +235,7 @@ def HoG(img, cell_per_blk=(3,3), pix_per_cell=(8, 8), orientation = 3):
     normalised_blocks = np.zeros((n_blocksy, n_blocksx, by, bx, orientation))
     for x in range(n_blocksx):
         for y in range(n_blocksy):
-            block = ori_histogram[y:y + by, x:x + bx, :]
+            block = ori_histogram[y : y + by, x : x + bx, :]
             eps = 1e-5
             normalised_blocks[y, x, :] = block / sqrt(block.sum() ** 2 + eps)
 
