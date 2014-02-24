6810e57ff47acc469adc32fbc4ae112f47e367c2
enthalpy-yan <enthalpy.yan@gmail.com>
2014-02-23 21:35:16 -0500

    Changed file structure

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
-import Descriptors as des
-import DistanceMeasures as dm
-import SimilarityMeasures as sm
+import lib.Descriptors as des
+import lib.DistanceMeasures as dm
+import lib.SimilarityMeasures as sm
 
 if __name__ == "__main__":
+    print "Test start..."
