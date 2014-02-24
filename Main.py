1ff7fdf5f9580f051dc290c7d66a82c451315903
minhui <guminhui@hotmail.com>
2014-02-24 11:21:34 -0500

    add test case for histogram similarity 	modified:   Main.py 	new file:   bunny.jpg

--- Main.py
+++ Main.py
@@ -14,8 +14,12 @@ if __name__ == "__main__":
     transformed_img = des.lbp(img1)
     cv2.imshow('image', img1)
     cv2.imshow('thresholded image', transformed_img)
-    cv2.waitKey(50)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+    print "Testing HOG"
+    hog = des.HoG(img1,See_graph=True)
+    print hog
 
     print "Testing distance calculation functions"
     print "L1 distance is " + str(dm.L1Dist(img1, img2))
@@ -23,6 +27,7 @@ if __name__ == "__main__":
     print "Chi-square distance is " + str(dm.ChiSquareDist(img1, img2))
     print "KL distance is " + str(dm.KLDist(img1, img2))
 
-    print "Testing HOG"
-    hog = des.HoG(img1,See_graph=True)
-    print hog
+    img3 = cv2.imread('bunny.jpg', 0)
+    img3 = des.Resize(img3,img1.shape[0],img1.shape[1])
+    print "Histogram Intersection similarity of the same img is ", float(sm.hist_intersection(img1, img2))
+    print "Histogram Intersection similarity of 2 different samples is ", float(sm.hist_intersection(img1, img3))
