85ead15ffeb6454658b0528f6a4125d35af64d6b
enthalpy-yan <enthalpy-yan@users.noreply.github.com>
2014-02-23 21:44:44 -0500

    Update SimilarityMeasures.py

--- lib/SimilarityMeasures.py
+++ lib/SimilarityMeasures.py
@@ -1,12 +1,25 @@
 import numpy as np
 
 def CosineSimilarity(i1, i2):
+    """
+    Function for calculating cosine similarity between two image
+    
+    i1, i2:
+     numpy ndarray for image
+     
+    """
     numerator = (i1 * i2).sum()
     denoma = (i1 * i1).sum()
     denomb = (i2 * i2).sum()
     return 1 - numerator / np.sqrt(denoma*denomb)
 
-def hist_intersection(A, B):	
+def hist_intersection(A, B):
+    """
+    Function for calculating histogram intersection between two image
+    
+    A, B:
+     numpy ndarray for image
+    """
     min_matrix = np.where(A >= B, B, 0) + np.where(A < B, A, 0)
     the_min = min_matrix / float(min(np.sum(A.ravel()), np.sum(B.ravel())))
     return sum(the_min.ravel())
